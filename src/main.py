7abf3dce3e87fa3527c4505bb76415d65dc279bb
Edgar <avila.agramonte.edg@gmail.com>
2022-09-14 20:58:14 -0500

    Added 2 more shade levels

--- src/main.py
+++ src/main.py
@@ -6,15 +6,25 @@ def main():
     # Parse args
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--filename', help='Name of the video that you want to pass to ascii')
+    parser.add_argument('-s', '--shade', help='Level of shading: "binary", "normal", "extended"')
     args = parser.parse_args()
 
     # Check args
     if args.filename is None:
         raise RuntimeError('You need to specify a filename')
+    if args.shade is not None:
+        if args.shade not in ('binary', 'normal', 'extended'):
+            raise RuntimeError('The argument shade should be "binary", "normal" or "extended"')
 
     # Define variables
     filename = args.filename
-    shades = ' .:-=+*#%@'
+    selected_shade_level = args.shade or 'normal'
+    shade_levels = {
+        'binary': '.@',
+        'normal': ' .:-=+*#%@',
+        'extended': ' .`^",:;Il!i><~+_-?][1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+    }
+    shades = shade_levels[selected_shade_level]
 
     # Get video
     video = cv.VideoCapture(filename)
